e65a90094020a56dec8c9e3d3847b99cd45a36b5
Muhammad Hassaan Attari <hassaanattari06@gmail.com>
2026-08-16 16:09:58 +0500

    Updated the file

--- src/components/ui/RegionCard.tsx
+++ src/components/ui/RegionCard.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { Language } from '@/types/i18n';
-import RegionCanvasPattern, { RegionId } from './RegionCanvasPattern';
+import { RegionId } from './RegionCanvasPattern';
 import { Sparkles } from 'lucide-react';
+
+const RegionCanvasPattern = dynamic(() => import('./RegionCanvasPattern'), {
+  ssr: false,
+});
 
 interface RegionCardProps {
   id: RegionId;
